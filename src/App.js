b5b7d61b9126eb98d301fc9d068a95950ca4a15e
Pendeks <Arthaka46@mail.ugm.ac.id>
2022-07-08 11:11:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component}from'react'
+class App extends Component{
+  constructor() {
+    super()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    this.state = {
+      name: 'Asen'
+    }
+  }
+    render(){
+      const name = this.state.name
+      return(
+      <div>
+        <div>My name is {name}</div>
+      </div>
+      )
+    }
+  }
+        
 export default App;
